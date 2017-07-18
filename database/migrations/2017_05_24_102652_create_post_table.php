6029450881c8ed7d491c4ccecd7a00fb72ea4327
Rakshya <rakshya@studiomatrix.com.np>
2017-07-18 10:19:35 +0545

    my first commit

---
+++ database/migrations/2017_05_24_102652_create_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('body');
+            $table->integer('author_id')->unsigned();//->comment('id of the author who created this post');
+            $table->foreign('author_id')->references('id')->on('users');//->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
